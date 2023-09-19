bd872b8a720154bedad2e2743db60281f7a48b70
ari5008 <aridayo.0428.good.yes@ezweb.ne.jp>
2023-09-20 08:15:00 +0900

    feat: HashFunc追加

--- main.go
+++ main.go
@@ -1,89 +1,20 @@
-// package main
-
-// import "fmt"
-
-// type People struct {
-// 	Number      int
-// 	Old         int
-// 	Children    bool
-// 	AlcoholFlag bool
-// 	TotalPrice  int
-// }
-
-// func main() {
-// 	var (
-// 		N int
-// 		K int
-// 	)
-// 	fmt.Scan(&N, &K)
-
-// 	peoples := []People{}
-// 	for i := 0; i <= N-1; i++ {
-// 		var old int
-// 		fmt.Scan(&old)
-// 		if old >= 20 {
-// 			people := People{
-// 				Number:   i + 1,
-// 				Old:      old,
-// 				Children: false,
-// 			}
-// 			peoples = append(peoples, people)
-// 		} else {
-// 			people := People{
-// 				Number:   i + 1,
-// 				Old:      old,
-// 				Children: true,
-// 			}
-// 			peoples = append(peoples, people)
-// 		}
-// 	}
-
-// 	for i := 0; i <= K-1; i++ {
-// 		var (
-// 			num   int
-// 			dish  string
-// 			price int
-// 		)
-// 		fmt.Scan(&num, &dish, &price)
-
-// 		if dish == "softdrink" {
-// 			peoples[num-1].TotalPrice += price
-// 		} else if dish == "food" {
-// 			if peoples[num-1].AlcoholFlag {
-// 				peoples[num-1].TotalPrice += price - 200
-// 			} else {
-// 				peoples[num-1].TotalPrice += price
-// 			}
-// 		} else if dish == "alcohol" {
-// 			if peoples[num-1].Children {
-// 				return
-// 			} else {
-// 				peoples[num-1].TotalPrice += price
-// 				peoples[num-1].AlcoholFlag = true
-// 			}
-// 		}
-// 	}
-
-// 	for _, people := range peoples {
-// 		fmt.Println(people.TotalPrice)
-// 	}
-
-// }
-
 package main
 
-import try "backend/try/list"
-
+import (
+	try "backend/try/hash"
+	"fmt"
+)
 
 func main() {
-	ll := new(try.DoubleLinkedList)
+	hashTable := try.NewHashTable(10)
+	hashTable.Add("key1", "value1")
+	hashTable.Add("key2", "value2")
+	hashTable.Add("key3", "value3")
 
-	// ll.Append(2)
-	// ll.Append(3)
-	ll.Insert(9)
-	ll.Insert(2)
-	ll.Insert(2)
-	// ll.Remove(3)
-	ll.Display()
+	hashTable.Print()
 
+	fmt.Println(hashTable.Get("key1"))
+	fmt.Println(hashTable.Get("key2"))
+	fmt.Println(hashTable.Get("key3"))
+	fmt.Println(hashTable.Get("key4"))
 }
